0acf74b4a20632d6ced22370dc833d2190771c14
Ali Zaidi <msazaidi@gmail.com>
2015-03-08 17:06:55 -0700

    Initial Commit

---
+++ directory_scanner.rb
@@ -0,0 +1,50 @@
+require 'open-uri'
+
+puts "\nWELCOME TO THE DIRECTORY SCANNER\n"
+
+def scan
+	puts "Please type in the name of the Directory you'd like to scan.\n"
+	print ">. "
+
+	dirname = gets.chomp
+
+	if dirname.empty?
+		dirname = "hardware"
+		puts "No Input Found Defaulting to Hardware Directory"
+	end
+
+	puts "What Phrase would you like to scan for? (NOTE: Phrase is Case Sensitive)"
+	print ">. "
+
+	phrase = gets.chomp
+	results = File.open("#{phrase}_results.csv", "w")
+	total_matches = 0
+
+	total_directories = Dir.glob("#{dirname}/**/*.*").each do |fname|
+		file_read_contents = File.open(fname).read
+		if file_read_contents.match(phrase)
+			results.puts "#{fname}\t\t\t FOUND MATCH FOR PHRASE [ #{phrase} ]"
+			total_matches+=1
+		end
+	end
+
+	puts "\n=============================================================================="
+	puts "\n                               SCAN RESULTS                                   "
+	puts "\n=============================================================================="
+	puts "\nScanned a Total of #{total_directories.length} files within #{dirname} directory and sub-directories."
+	puts "Crunching Numbers ... <insert robot noises> ... "
+	puts "Found #{total_matches} Incidents of #{phrase} within #{dirname} Directory from a Total of #{total_directories.length} Files"
+	puts ""
+	puts "Printing Results to #{phrase}_Results.csv"
+
+	puts "\nWould you like to scan another term or phrase?"
+	puts "Please Type Yes or No"
+	input = gets.chomp.capitalize
+	if (input == "Yes")
+		scan
+	else (input == "No")
+		puts "Thank you for using the Document Scanner. Have a wonderful day and day."
+	end
+end
+
+scan
---
+++ scraping.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+ 
+class Post
+  attr_accessor :title, :url, :points, :item_id, :num_comments, :comment_list
+ 
+  def initialize(url)
+    @doc = Nokogiri::HTML(open(url))
+    @comment_list = []
+  end
+ 
+  def scrape_post_information
+    @title = @doc.search('.title > a:first-child').map {|link| link.inner_text}
+    @url = @doc.search('.title > a:first-child').map {|link| link['href']}
+    @points = @doc.search('.subtext > span:first-child').map { |span| span.inner_text } 
+    @num_comments = @doc.search('.subtext > a:nth-child(3)').map {|link| link.inner_text }
+	  item_id_href = @doc.search('.subtext > a:nth-child(3)').map {|link| link['href'] }	
+    @item_id = item_id_href[0].scan(/\d+/)
+  end
+ 
+  def create_comment_list
+	comments_strings = @doc.search('.comment').map {|comment| comment.inner_text } # Array of Strings
+	comments_strings.each { |comments| @comment_list << Comment.new(comments)}
+  end
+ 
+  def add_comment(string)
+	  @comment_list << Comment.new(string)
+  end
+ 
+  def comments
+	  @comment_list
+  end
+ 
+end
+ 
+  class Comment
+	def initialize(string)
+	  @comment_text = string
+  end
+end
+ 
+url = ARGV[0]
+post = Post.new
+post.scrape_post_information
+p post.title.first
+p post.url.first
+p post.points.first
+p post.num_comments.first
+p post.item_id.first
+ 
+puts "comments are below: "
+post.create_comment_list
+post.add_comment("donkeys are the best")
+p post.comments
